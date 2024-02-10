8a26b041441ea205e51e68dccb8c61802084e47d
Justin Hold <36420615+jhold1982@users.noreply.github.com>
2024-02-10 13:28:17 -0600

    Initial Commit

--- SymbolColoring/ContentView.swift
+++ SymbolColoring/ContentView.swift
@@ -9,13 +9,42 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+		ZStack {
+			LinearGradient(
+				colors: [.white, .gray],
+				startPoint: .top,
+				endPoint: .bottom
+			)
+			.ignoresSafeArea()
+			
+			VStack {
+				Label("Vision Pro", systemImage: "apple.logo")
+					.font(.largeTitle)
+					.fontWeight(.medium)
+				Image(systemName: "visionpro")
+					.font(
+						.system(size: 300, weight: .ultraLight)
+					)
+					.foregroundStyle(
+						LinearGradient(
+							colors: [.gray, .black, .gray],
+							startPoint: .top,
+							endPoint: .bottom
+						),
+						EllipticalGradient(
+							colors: [.purple, .black],
+							center: .center,
+							startRadiusFraction: 0.0,
+							endRadiusFraction: 0.55
+						)
+					)
+					.shadow(
+						color: .black,
+						radius: 25,
+						y: 7
+					)
+			}
+		}
     }
 }
 
