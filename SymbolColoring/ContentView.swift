18d156c2d33ae34d50069ffa62b3008595916aca
Justin Hold <36420615+jhold1982@users.noreply.github.com>
2024-02-10 13:19:31 -0600

    Initial Commit

---
+++ SymbolColoring/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SymbolColoring
+//
+//  Created by Justin Hold on 2/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SymbolColoring/SymbolColoringApp.swift
@@ -0,0 +1,17 @@
+//
+//  SymbolColoringApp.swift
+//  SymbolColoring
+//
+//  Created by Justin Hold on 2/10/24.
+//
+
+import SwiftUI
+
+@main
+struct SymbolColoringApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
